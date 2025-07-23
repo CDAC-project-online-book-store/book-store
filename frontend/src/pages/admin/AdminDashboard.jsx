dd6661b31a81827021875aa4c522fd3f8bc7cd5f
Anurag Kaushal <work.anuragkaushal@gmail.com>
2025-07-23 18:17:21 +0530

    added admin pages

---
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function AdminDashboard() {
+  return (
+   <div className='container mt-4'>
+      <h2>Welcome, Admin</h2>
+      <p className="lead">select a module to manage: </p>
+        <div className="list-group">
+            <Link to="/admin/books">Manage Book</Link>
+            <Link to="/admin/users">Manage User</Link>
+            <Link to="/admin/orders">Manage Order</Link>
+            <Link to="/admin/analytics">Analytics</Link>    
+        </div>
+    </div>
+  )
+}
+
+export default AdminDashboard
---
+++ frontend/src/pages/admin/Analytics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Analytics() {
+  return (
+    <div>
+      <h2>Analytics page</h2>
+    </div>
+  )
+}
+
+export default Analytics
