73d14b228a5eac6d0bc8e0b62486312a115baf4a
Tejas73 <tejasbhoyar5@gmail.com>
2025-07-25 09:45:55 +0530

    used props in AddressBox component

--- frontend/src/components/AddressBox.jsx
+++ frontend/src/components/AddressBox.jsx
@@ -1,21 +1,12 @@
-import { useNavigate } from "react-router-dom";
-
-function AddressBox() {
-
-    const navigate = useNavigate();
-    
-    const handleAddClick = () => {
-    navigate('/add-new-address');
-    };
-    
+function AddressBox({ title, onClickHandler }) {
     return (<div className="col-md-4">
         <div
             className="card text-center border-dashed p-4 h-100 d-flex align-items-center justify-content-center"
             style={{ cursor: "pointer", border: "2px dashed #38a3a5", minHeight: "200px" }}
-            onClick={handleAddClick}
+            onClick={onClickHandler}
         >
             <div style={{ fontSize: '2rem', color: '#38a3a5' }}>+</div>
-            <div>Add Address</div>
+            <div>{title}</div>
         </div>
     </div>);
 }
