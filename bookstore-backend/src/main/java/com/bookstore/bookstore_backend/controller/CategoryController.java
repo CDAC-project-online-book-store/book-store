5526a22af27a168932e5f65f982d111130c27944
Ashwin Udmale <ashwinudmale009@gmail.com>
2025-08-06 15:49:25 +0530

    added controllers and their functionalities, configs

--- bookstore-backend/src/main/java/com/bookstore/bookstore_backend/controller/CategoryController.java
+++ bookstore-backend/src/main/java/com/bookstore/bookstore_backend/controller/CategoryController.java
@@ -1,5 +1,28 @@
 package com.bookstore.bookstore_backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookstore.bookstore_backend.dto.CategoryDTO;
+import com.bookstore.bookstore_backend.service.CategoryService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/categories")
+@AllArgsConstructor
 public class CategoryController {
 
+	private final CategoryService categoryService;
+
+	@PostMapping("/{bookId}")
+	public ResponseEntity<CategoryDTO> addCategory(@PathVariable Long bookId, @RequestBody CategoryDTO dto) {
+		CategoryDTO createdCategory = categoryService.addCategoryToBook(bookId, dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+	}
 }
--- bookstore-backend/src/main/java/com/bookstore/bookstore_backend/service/AuthorServiceImpl.java
+++ bookstore-backend/src/main/java/com/bookstore/bookstore_backend/service/AuthorServiceImpl.java
@@ -4,13 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bookstore.bookstore_backend.custom_exceptions.ResourceNotFoundException;
 import com.bookstore.bookstore_backend.dao.AuthorDao;
 import com.bookstore.bookstore_backend.dao.BookDao;
 import com.bookstore.bookstore_backend.dto.AuthorDTO;
 import com.bookstore.bookstore_backend.entities.AuthorEntity;
+import com.bookstore.bookstore_backend.entities.BookEntity;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -19,19 +20,31 @@ import lombok.AllArgsConstructor;
 @Transactional
 @AllArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
-	@Autowired
-	private AuthorDao authorDao;
 
-	@Autowired
-	private ModelMapper modelMapper;
+    private final AuthorDao authorDao;
+    private final BookDao bookDao;
+    private final ModelMapper modelMapper;
 
-	@Autowired
-	private BookDao bookDao;
+    @Override
+    public List<AuthorDTO> searchAuthorsByName(String name) {
+        List<AuthorEntity> entities = authorDao.findByAuthorContainingIgnoreCase(name);
+        return entities.stream()
+                .map(author -> modelMapper.map(author, AuthorDTO.class))
+                .collect(Collectors.toList());
+    }
 
-	@Override
-	public List<AuthorDTO> searchAuthorsByName(String name) {
-		List<AuthorEntity> entities = authorDao.findByAuthorContainingIgnoreCase(name);
-		return entities.stream().map(author -> modelMapper.map(author, AuthorDTO.class)).collect(Collectors.toList());
-	}
+    @Override
+    public AuthorDTO addAuthorToBook(Long bookId, AuthorDTO dto) {
+        BookEntity bookEntity = bookDao.findById(bookId)
+                .orElseThrow(() -> new ResourceNotFoundException("Invalid book id - authors cannot be assigned"));
+        AuthorEntity authorEntity = modelMapper.map(dto, AuthorEntity.class);
 
+        // Set both sides of the relationship
+        bookEntity.addAuthor(authorEntity);
+
+        // Save the author (or book, either works due to cascade)
+        authorDao.save(authorEntity);
+
+        return modelMapper.map(authorEntity, AuthorDTO.class);
+    }
 }
