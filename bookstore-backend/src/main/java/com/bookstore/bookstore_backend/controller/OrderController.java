2c7e7cb5d7c6d4788a6eaccbdc9110b8662007d5
Anuvrat <anuvratarya1504@gmail.com>
2025-08-10 01:12:30 +0530

    feat(order): add endpoint to retrieve all orders, GET /orders endpoint to fetch all orders from the database.

--- bookstore-backend/src/main/java/com/bookstore/bookstore_backend/controller/OrderController.java
+++ bookstore-backend/src/main/java/com/bookstore/bookstore_backend/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.bookstore.bookstore_backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +15,25 @@ import com.bookstore.bookstore_backend.dto.OrderRequestDTO;
 import com.bookstore.bookstore_backend.service.OrderService;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/order")
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class OrderController {
 
-	@Autowired
-	private OrderService orderService;
+	private final OrderService orderService;
 
 	@PostMapping
 	public ResponseEntity<OrderDTO> placeOrder(@RequestBody @Valid OrderRequestDTO request) {
 		OrderDTO response = orderService.createOrder(request);
 		return ResponseEntity.ok(response);
 	}
+
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> getAllOrders() {
+		return new ResponseEntity<>(orderService.getAllOrders(), HttpStatus.OK);
+	}
+
 }
