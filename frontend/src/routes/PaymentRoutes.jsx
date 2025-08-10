6c6dd65be2db52ae94a7e51d0920ba356fd8c8b6
Ashwin Udmale <ashwinudmale009@gmail.com>
2025-08-10 12:10:42 +0530

    Add protected routes for checkout and order placement pages

--- frontend/src/routes/PaymentRoutes.jsx
+++ frontend/src/routes/PaymentRoutes.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Cart from '../pages/payment/Cart';
 import Checkout from '../pages/payment/Checkout';
+import ProtectedRoute from './ProtectedRoute';
+import OrderPlaced from '../pages/payment/OrderPlaced';
 
 
 function PaymentRoutes() {
   return (
     <Routes>
       <Route path="/payment/cart" element={<Cart />} />
-      <Route path="/payment/checkout" element={<Checkout />} />
+      <Route path="/payment/checkout" element={<ProtectedRoute allowedRoles={["customer"]}><Checkout /></ProtectedRoute>} />
+      <Route path="/payment/order-placed" element={<ProtectedRoute allowedRoles={["customer"]}><OrderPlaced /></ProtectedRoute>} />
     </Routes>
   )
 }
